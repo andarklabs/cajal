e7f12d08a924d21772610b2033ffe73d484573a6
Andrew-Ceniccola <andrewsamce@me.com>
2023-11-10 10:27:39 -0500

    more setup...

--- src/dense.h
+++ src/dense.h
@@ -2,21 +2,24 @@
 #define DENSE_H
 
 #include <iostream>
+#include <stack>
+#include "perceptrons.h"
 
 using namespace std;
 
 class Dense{
 
 public:
-    Dense(int perceptrons); // Constructor
+    Dense(int num_perceptrons); // Constructor
     ~Dense(); // Destuctor
 
     void cleanLayer(); 
 
 
-
+    
 private:
 
+    stack<Perceptron*> layerStack;
 
 };
 #endif
