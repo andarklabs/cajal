515e5322cc4c415baeb996953069be7d63227ed3
Andrew-Ceniccola <andrewsamce@me.com>
2023-11-11 21:06:10 -0500

    mat math for 2dgit pull

--- src/math/mat_math.h
+++ src/math/mat_math.h
@@ -20,50 +20,65 @@ using Tensor = float*;
 using mat_size = size_t;
 
 
-Tensor createMatrix(mat_size N, mat_size M = NULL){
+Tensor createMatrix(mat_size n, mat_size m = NULL){
 
     // Check if we are working with square matrices
-    if (M == NULL){
-        M = N;
+    if (m == NULL){
+        m = n;
     };
 
     // not value initalized. 
-    Tensor C = new float[M*N];
+    Tensor C = new float[n*m];
 
     return C;
-
 }
 
-Tensor add(Tensor A, Tensor B, mat_size N){
+Tensor add(Tensor A, Tensor B, mat_size n){
+    Tensor C = createMatrix(n);
 
+	for (unsigned int i = 0; i < n; i++)
+		for (unsigned int j = 0; j < n; j++)
+			C[(i * n) + j] = A[(i * n) + j] + B[(i * n) + j];
+
+	return C;
 }
 
-Tensor sub(Tensor A, Tensor B, mat_size N){
+Tensor sub(Tensor A, Tensor B, mat_size n){
+    Tensor C = createMatrix(n);
 
+	for (unsigned int i = 0; i < n; i++)
+		for (unsigned int j = 0; j < n; j++)
+			C[(i * n) + j] = A[(i * n) + j] - B[(i * n) + j];
+
+	return C;
 }
 
-
+// matrix A has n rows and p columns and matrix B has p rows and m columns. 
+// The resultant matrix is n*m
 Tensor naive_mult(Tensor A, Tensor B, mat_size n, mat_size m, mat_size p) {
+    float sum = 0;
     Tensor C = createMatrix(n,p);
     for (int i = 0; i<n ; i++){
         for (int j = 0; j<m ; j++){
             for (int k = 0; k<p ; k++){
-
-            }
-        }
-    }
-
+                sum += A[(i * p) + k] * B[(k * m) + j];				// O(1)
+			}
+			C[(i * m) + j] = sum;							// O(1)
+			sum = 0;							// O(1)
+		}
+	}
+	return C;
 }
 
 
 // divide and conquer on square strassen 
-Tensor square_strassen(Tensor A, Tensor B, mat_size N){
+Tensor square_strassen(Tensor A, Tensor B, mat_size n){
 // TODO: Fill in with square strassen implementation
 
-    Tensor C = createMatrix(N);
+    Tensor C = createMatrix(n);
 
     // this is our base case i'm implored to understand
-	if (N == 1) {
+	if (n == 1) {
 
         C[0] = A[0] * B[0];
 
@@ -71,70 +86,71 @@ Tensor square_strassen(Tensor A, Tensor B, mat_size N){
 	}
 
     // sub matrix size 
-    mat_size K = N/2;
+    mat_size k = n/2;
 
     // we should create our sub matrices i guess
-    Tensor A11 = createMatrix(K);
-    Tensor A12 = createMatrix(K);
-    Tensor A21 = createMatrix(K);
-    Tensor A22 = createMatrix(K);
-    Tensor B11 = createMatrix(K);
-    Tensor B12 = createMatrix(K);
-    Tensor B21 = createMatrix(K);
-    Tensor B22 = createMatrix(K);
+    Tensor A11 = createMatrix(k);
+    Tensor A12 = createMatrix(k);
+    Tensor A21 = createMatrix(k);
+    Tensor A22 = createMatrix(k);
+    Tensor B11 = createMatrix(k);
+    Tensor B12 = createMatrix(k);
+    Tensor B21 = createMatrix(k);
+    Tensor B22 = createMatrix(k);
 
-    // filling in our matrices
-    for (unsigned int i = 0; i < K; i++) {
-            for (unsigned j = 0; j < K; j++) {
-                A11[i * K + j] = A[i * K + j];
-                A12[i * K + j] = A[(i * K) + K + j];		
-                A21[i * K + j] = A[((K + i) * K) + j];
-                A22[i * K + j] = A[((K + i) * K) + K + j];
-                B11[i * K + j] = B[i * K + j];
-                B12[i * K + j] = B[(i * K) + K + j];		
-                B21[i * K + j] = B[((K + i) * K) + j];
-                B22[i * K + j] = B[((K + i) * K) + K + j];
+    // filling in our matrices. We are using row-major order so C[i * N + j]
+    // means the ith row and the jth column
+    for (unsigned int i = 0; i < k; i++) {
+            for (unsigned j = 0; j < k; j++) {
+                A11[i * k + j] = A[i * k + j];
+                A12[i * k + j] = A[(i * k) + k + j];		
+                A21[i * k + j] = A[((k + i) * k) + j];
+                A22[i * k + j] = A[((k + i) * k) + k + j];
+                B11[i * k + j] = B[i * k + j];
+                B12[i * k + j] = B[(i * k) + k + j];		
+                B21[i * k + j] = B[((k + i) * k) + j];
+                B22[i * k + j] = B[((k + i) * k) + k + j];
             }
 	}
     
     // S
-	Tensor S1 = sub(B12, B22, K);
-	Tensor S2 = add(A11, A12, K);
-	Tensor S3 = add(A21, A22, K);
-	Tensor S4 = sub(B21, B11, K);
-	Tensor S5 = add(A11, A22, K);
-	Tensor S6 = add(B11, B22, K);
-	Tensor S7 = sub(A12, A22, K);
-	Tensor S8 = add(B21, B22, K);
-	Tensor S9 = sub(A11, A21, K);
-	Tensor S10 = add(B11, B12, K);
+	Tensor S1 = sub(B12, B22, k);
+	Tensor S2 = add(A11, A12, k);
+	Tensor S3 = add(A21, A22, k);
+	Tensor S4 = sub(B21, B11, k);
+	Tensor S5 = add(A11, A22, k);
+	Tensor S6 = add(B11, B22, k);
+	Tensor S7 = sub(A12, A22, k);
+	Tensor S8 = add(B21, B22, k);
+	Tensor S9 = sub(A11, A21, k);
+	Tensor S10 = add(B11, B12, k);
 
 	// P
-	Tensor P1 = square_strassen(A11, S1, K);
-	Tensor P2 = square_strassen(S2, B22, K);
-	Tensor P3 = square_strassen(S3, B11, K);
-	Tensor P4 = square_strassen(A22, S4, K);
-	Tensor P5 = square_strassen(S5, S6, K);
-	Tensor P6 = square_strassen(S7, S8, K);
-	Tensor P7 = square_strassen(S9, S10, K);
+	Tensor P1 = square_strassen(A11, S1, k);
+	Tensor P2 = square_strassen(S2, B22, k);
+	Tensor P3 = square_strassen(S3, B11, k);
+	Tensor P4 = square_strassen(A22, S4, k);
+	Tensor P5 = square_strassen(S5, S6, k);
+	Tensor P6 = square_strassen(S7, S8, k);
+	Tensor P7 = square_strassen(S9, S10, k);
 
 	// C submatrices
-	Tensor C11 = sub(add(add(P5, P4, K), P6, K), P2, K);				// P5 + P4 - P2 + P6
-	Tensor C12 = add(P1, P2, K);								        // P1 + P2
-	Tensor C21 = add(P3, P4, K);								        // P3 + P4
-	Tensor C22 = sub(sub(add(P5, P1, K), P3, K), P7, K);				// P1 + P5 - P3 - P7
+	Tensor C11 = sub(add(add(P5, P4, k), P6, k), P2, k);				// P5 + P4 - P2 + P6
+	Tensor C12 = add(P1, P2, k);								        // P1 + P2
+	Tensor C21 = add(P3, P4, k);								        // P3 + P4
+	Tensor C22 = sub(sub(add(P5, P1, k), P3, k), P7, k);				// P1 + P5 - P3 - P7
 
 	// build our C matrix										
-	for (unsigned int i = 0; i < K; i++) {
-		for (unsigned int j = 0; j < K; j++) {
+	for (unsigned int i = 0; i < k; i++) {
+		for (unsigned int j = 0; j < k; j++) {
 
-			C[i * K + j] = C11[i * K + j];
+			C[i * k + j] = C11[i * k + j];
 
-			C[(i * K) + K + j] = C12[i * K + j];
+			C[(i * k) + k + j] = C12[i * k + j];
 
-			C[((K + i) * K) + j] = C21[i * K + j];
+			C[((k + i) * k) + j] = C21[i * k + j];
 
-			C[((K + i) * K) + K + j] = C22[i * K + j];
+			C[((k + i) * k) + k + j] = C22[i * k + j];
 		}
 	}
 
